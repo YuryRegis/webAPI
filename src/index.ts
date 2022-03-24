351d3ae1fd274f3b5817de94e0bc3e668d521408
YuryRegis <yury_regis@hotmail.com>
2022-03-24 02:13:44 -0300

    Create Documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import heroesRoute from './routes/heroes'
+import swaggerUi from 'swagger-ui-express'
 
+
+import swaggerDocument from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', 
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument))
 
 app.use(heroesRoute)
 
